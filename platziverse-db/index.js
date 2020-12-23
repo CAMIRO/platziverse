a9a9fca5281f4953737fd0f0401fb87c1a1883d2
CAMIRO <juancamilosc@hotmail.com>
2020-12-23 12:34:00 +0800

    integrating db and setup file

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -5,17 +5,21 @@ const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
 
 module.exports = async function (config) {
-
   const sequelize = setupDatabase(config)
-  const AgentModel = setupMetricModel(config)
+  const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
 
+  // Relations
   AgentModel.hasMany(MetricModel)
   MetricModel.belongsTo(AgentModel)
 
   await sequelize.authenticate()
 
   // sequelize.sync()
+  if (config.setup) {
+    // force: true: siginifica que si la base de datos existe, borrela y cree una nueva.
+    await sequelize.sync({ force: true })
+  }
 
   const Agent = {}
   const Metric = {}
